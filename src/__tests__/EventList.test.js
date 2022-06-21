51f0e62d4688294b9727afdca4650524c8ce0001
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-21 15:53:26 +0200

    Update test EventList

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import EventList from '../Components/EventList';
+import App from '../Components/App';
 
 describe('EvenList component', () => {
   test('render correct number of events', () => {
-    const { result } = render(< EventList events={
-      [{}, {}, {}, {}]}
-    />);
-
-    expect(result).toHaveLength(4);
+    render(
+      <EventList events={
+        [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
+      />
+    );
+    const ListItem = screen.getAllByRole('listitem')
+    expect(ListItem).toHaveLength(4);
   });
 })
