8a2c7058badcfd6824c8899eea5889d2b154421a
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-21 14:59:50 +0200

    Move to Components folder

---
+++ src/Components/EventList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Event from '../Components/Event'
+
+class EventList extends Component {
+  render() {
+    const { events } = this.props;
+
+    return (
+      < ul className='EventList' >
+        {
+          events.map(event =>
+            <li>
+              <Event event={event} />
+            </li>
+          )
+        }
+        < /ul>
+        )
+  }
+};
+
+        export default EventList;
