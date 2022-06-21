cb226f359a2e8e29a40fb2d94066ef93fb2aaadb
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-21 13:59:44 +0200

    Create and implement EventList Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import EventList from './EventList'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       < div title='main' className='App' >
-
+        <EventList data-testid='eventList' />
       </div>
 
     );
