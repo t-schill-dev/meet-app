579ee7b1a81b7e7cd5f07c34bb7f251c3ede2f15
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-22 13:49:27 +0200

    Implement enzyme to EventList test

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
 import EventList from '../Components/EventList/EventList';
+import Event from '../Components/Event/Event'
 
 describe('EvenList component', () => {
   test('render correct number of events', () => {
-    render(
+    const EventListWrapper = shallow(
       <EventList events={
         [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
       />
     );
-    const ListItem = screen.getAllByRole('listitem')
-    expect(ListItem).toHaveLength(4);
+    expect(EventListWrapper.find(Event)).toHaveLength(4);
   });
 })
