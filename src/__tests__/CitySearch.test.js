0455434d662aad94cc9671798b76499f5f866ad2
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-22 14:51:27 +0200

    Adding test for input field

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -12,5 +12,11 @@ describe('<CitySearch/> Component', () => {
   });
   test('renders list of suggestions', () => {
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+  });
+  test('renders text input correctly', () => {
+    //test passes when user types into textbox and changing state to query
+    //compare prop values of .city elements with the query
+    const query = CitySearchWrapper.state('query');
+    expect(CitySearchWrapper.find('.city').prop('value')).toBe(query)
   })
 });
