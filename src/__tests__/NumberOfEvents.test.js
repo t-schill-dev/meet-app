8eb5bc81eb6dad9831effb7b06394011b86fb1f3
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-24 15:02:00 +0200

    Create test to render text input

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -3,8 +3,26 @@ import { shallow } from 'enzyme';
 import NumberOfEvents from '../Components/NumberOfEvents';
 
 describe('<NumberOfEvents/> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />)
+  })
+
   test('render correct input number', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find('.number').length).toBe(1);
   })
+  test('renders text input correctly', () => {
+    //test passes when user types into textbox and changing state to query
+    //compare prop values of .city elements with the query
+    const query = NumberOfEventsWrapper.state('query');
+    expect(NumberOfEventsWrapper.find('.number').prop('value')).toBe(query)
+  })
+  // test('change state when input changes', () => {
+  //   CitySearchWrapper.setState({
+  //     query: 'Munich'
+  //   });
+  //   const eventObject = { target: { value: 'Berlin' } };
+  //   CitySearchWrapper.find('.city').simulate('change', eventObject);
+  //   expect(CitySearchWrapper.state('query')).toBe('Berlin');
+  // })
 })
