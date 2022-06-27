ff8b4219ebd86c8454c691c82ab878a36055b364
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-27 15:26:35 +0200

    Add test of matching list

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,6 +4,9 @@ import App from '../Components/Main/App';
 import EventList from '../Components/EventList/EventList';
 import CitySearch from '../Components/CitySearch/CitySearch';
 import NumberOfEvents from '../Components/NumberOfEvents';
+import { mockData } from '../../src/mock-data';
+import { extractLocations } from '../../src/api';
+import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
 
 
 describe('<App/> component', () => {
@@ -35,12 +38,23 @@ describe('<App/> integration', () => {
         const AppEventsState = AppWrapper.state('events');
         expect(AppEventsState).not.toEqual(undefined);
         expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
-
     });
     test('App passes "locations" state as prop to CitySearch', () => {
         const AppLocationsState = AppWrapper.state('locations');
         expect(AppLocationsState).not.toEqual(undefined);
         expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
-
+    })
+    test('get list of events matching the city selected', async () => {
+        const CitySearchWrapper = AppWrapper.find(CitySearch);
+        const locations = extractLocations(mockData);
+        CitySearchWrapper.setState({ suggestions: locations });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        const selectIndex = Math.floor(Math.random() * (suggestions.length));
+        const selectedCity = suggestions[selectIndex];
+        //Instance method allows to call any function from component
+        await CitySearchWrapper.instance().handleItemClick(selectedCity);
+        const allEvents = await getEvents();
+        const eventsToShow = allEvents.filter(event => event.location === selectedCity);
+        expect(AppWrapper.state('events')).toEqual(eventsToShow);
     })
 })
