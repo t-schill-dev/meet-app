3a0df634358e7cc2c78cdd3febe39144d1e99838
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-28 10:41:58 +0200

    Add data to event component

--- src/Components/Event/Event.js
+++ src/Components/Event/Event.js
@@ -7,28 +7,35 @@ class Event extends Component {
       visible: false
     }
   }
-
+  convertDateEvent = (date) => {
+    return new Date(date).toLocaleDateString()
+  }
+  convertTimeEvent = (date) => {
+    return new Date(date).toLocaleTimeString()
+  }
   handleShowDetails = (prevState) => {
     this.setState({
       visible: !prevState.visible
     })
   }
+
   render() {
-    return (
-      <div className='event'>
-        <div className='summary'></div>
-        <div className='dateTime'></div>
-        <div className='location'></div>
-        {this.state.visible ? (
-          <p className='eventDescription'>{this.props.event.description}</p>
-        ) : null}
-        {/*Render hide details button when eventDescription is rendered*/}
-        <button
-          className='button'
-          onClick={() => this.handleShowDetails(this.state)}>
-          {this.state.visible ? 'Hide details' : 'Show details'}
-        </button>
-      </div >
+    return (<div className='event' >
+      <div className='summary'>{this.props.event.summary}</div>
+      <div className='dateTime'>
+        {this.convertDateEvent(this.props.event.start.dateTime)}
+        <p>{this.convertTimeEvent(this.props.event.start.dateTime)}</p>
+        <p>- {this.convertTimeEvent(this.props.event.end.dateTime)}</p>
+      </div>
+      <div className='location'>{this.props.event.location}</div>
+      {this.state.visible ? (
+        <p className='eventDescription' > {this.props.event.description} </p>
+      ) : null
+      } { /*Render hide details button when eventDescription is rendered*/}
+      <button className='button'
+        onClick={
+          () => this.handleShowDetails(this.state)} > {this.state.visible ? 'Hide details' : 'Show details'} </button>
+    </div >
     )
   }
 }
