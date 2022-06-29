12f950f08664999016ee3934c0e6202f9763f0f8
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-06-29 10:55:49 +0200

    Minor changes

--- src/api.js
+++ src/api.js
@@ -62,7 +62,7 @@ const getToken = async(code) => {
 
     try {
         const encodeCode = encodeURIComponent(code);
-        const response = await fetch('https://d256su3iob.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + encodeCode)
+        const response = await fetch(`https://d256su3iob.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
