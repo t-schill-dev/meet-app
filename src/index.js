c4c0158f4589f39d5782f04273998290f170c2e3
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-07-07 11:07:56 +0200

    Implement atatus browser testing

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('352b92ee908543b184bc9da90f750d6c').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode >
     <App />
   </React.StrictMode>
 );
@@ -21,3 +23,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
