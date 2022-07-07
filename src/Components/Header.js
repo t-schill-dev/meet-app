ba63ab1ac701712535cb81dc0d2fe8a21bf98642
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-07-07 09:43:43 +0200

    Resolve version react to 17 and delete RTL

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 import { BsFillPeopleFill } from 'react-icons/bs';
 
 export default class Header extends Component {
   render() {
-    return (
-      <Navbar>
-        <Container>
-          <div className="header">
-            <BsFillPeopleFill />{' '}
-            Meet App - Meet other people while learning what you love!
-          </div>
-        </Container>
-      </Navbar>
+    return (<Navbar >
+      <Container >
+        <div className="header" >
+          <BsFillPeopleFill /> {' '}
+          Meet App - Meet other people
+          while learning what you love!
+        </div>
+      </Container>
+    </Navbar>
     )
   }
 }
