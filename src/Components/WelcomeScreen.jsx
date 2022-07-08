c715c0351759e625e72a18356dc9feebd9c4d9dc
tschill859 <100695942+t-schill-dev@users.noreply.github.com>
2022-07-08 14:48:50 +0200

    Styling

--- src/Components/WelcomeScreen.jsx
+++ src/Components/WelcomeScreen.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
+import { Container, Button } from 'react-bootstrap';
 
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
     (
-      <div className="WelcomeScreen">
-        <h1>Welcome to the Meet app</h1>
-        <h4>
-          Log in to see upcoming events around the world for
-          full-stack
-          developers
-        </h4>
-        <div className="button_cont" align="center">
-          <div class="google-btn">
-            <div class="google-icon-wrapper">
-              <img
-                class="google-icon"
-                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-  o.svg"
-                alt="Google sign-in"
-              />
+      <>
+        <Container className="p-5 m-5">
+          <div className="WelcomeScreen">
+            <h1>Welcome to the Meet app</h1>
+            <h4>
+              Log in to see upcoming events around the world for full-stack developers
+            </h4>
+            <div className="button_cont" align="center">
+              <div className="google-btn">
+                <div className="google-icon-wrapper">
+                  <img className="google-icon"
+                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                    alt="Google sign-in"
+                  />
+                </div>
+                <Button variant='primary' onClick={() => { props.getAccessToken() }}
+                  rel="nofollow noopener"
+                  className="login-btn-text"
+                >
+                  <b>Sign in with google</b>
+                </Button>
+              </div>
             </div>
-            <button onClick={() => { props.getAccessToken() }}
+            <a
+              href="../../public/privacy.html"
               rel="nofollow noopener"
-              class="btn-text"
             >
-              <b>Sign in with google</b>
-            </button>
+              Privacy policy
+            </a>
           </div>
-        </div>
-        <a
-          href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
-          rel="nofollow noopener"
-        >
-          Privacy policy
-        </a>
-      </div>
+        </Container>
+      </>
     )
     : null
 }
